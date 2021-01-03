219e4e5b0ca55fc28eb08a263e84213062cdf67c
iolanda.rosa <iolanda.rosa@voidsoftware.com>
2021-01-03 22:20:16 +0000

    js server updated

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const WebSocket = require("ws");
+const { Server } = require('ws');
 
-const wss = new WebSocket.Server({ server });
+const wss = new Server({ server });
 
-wss.on("connection", ws => {
-  ws.on("message", message => {
-    ws.send("Echo message sent back from server: " + message);
-  });
-  ws.send("Hello! Message from server. You've just connected to the server!!");
-
+wss.on('connection', (ws) => {
+  console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
 });
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
 
